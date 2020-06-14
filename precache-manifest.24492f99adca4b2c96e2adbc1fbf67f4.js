758021be445e4553ee2659ddb476ec8c4f46304e
yishuenlo <lo.yishuen@gmail.com>
2020-06-14 06:53:18 -0600

    Updates

---
+++ precache-manifest.24492f99adca4b2c96e2adbc1fbf67f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71703181b225ddba961aeba08a2a660a",
+    "url": "/smart-brain/index.html"
+  },
+  {
+    "revision": "96f7697ee092a0d9eec1",
+    "url": "/smart-brain/static/css/main.4f7f98a9.chunk.css"
+  },
+  {
+    "revision": "92891a0686315b0e16f8",
+    "url": "/smart-brain/static/js/2.f2333aa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smart-brain/static/js/2.f2333aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96f7697ee092a0d9eec1",
+    "url": "/smart-brain/static/js/main.a217da35.chunk.js"
+  },
+  {
+    "revision": "f713852481949665bf23",
+    "url": "/smart-brain/static/js/runtime-main.850e91ee.js"
+  }
+]);
