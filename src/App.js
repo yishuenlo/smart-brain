f835187fe21a8a837cf5e0bfdc8946eea0a584eb
yishuenlo <lo.yishuen@gmail.com>
2020-06-14 06:41:44 -0600

    refine design

--- src/App.js
+++ src/App.js
@@ -2,38 +2,14 @@ import React from "react";
 import Navigation from "./components/Navigation/Navigation";
 import AppWrapper from "./components/AppWrapper/AppWrapper";
 import Particles from "react-particles-js";
+import ParticlesSetting from "./components/particlesJS.json";
 
 import "./App.css";
 
 function App() {
-  const particlesOptions = {
-    particles: {
-      number: {
-        value: 200,
-        density: {
-          enable: true,
-          value_area: 1000,
-        },
-      },
-    },
-    shape:{
-      type: 'cirlces',
-      stroke: {
-        width: 0,
-      }
-    },
-    line_linked: {
-      opacity: 0.015,
-    },
-    opacity: {
-      value: 0.5,
-      random: true
-    }
-  };
-
   return (
     <div className="App">
-      <Particles className="particles" params={particlesOptions} />
+      <Particles className="particles" params={ParticlesSetting} />
       <Navigation />
       <AppWrapper />
     </div>
